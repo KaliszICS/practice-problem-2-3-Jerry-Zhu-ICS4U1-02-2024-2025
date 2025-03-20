12082f20890b693fbf27ec94618252520ce7c683
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-03-20 20:44:47 +0000

    Finish practice problems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,24 +4,24 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+	public static void bubbleSortString(String[] strings) {
+		boolean hasSwitched = true;
+		String temp;
+		//for(int i = 0; i < strings.length; i++){
+		//	strings[i] = strings[i].toLowerCase();		
+		//}
+		for(int i = 0; i < strings.length - 1 && hasSwitched; i++){
+			hasSwitched = false;
+			for(int j = 0; j < strings.length - 1 - i; j++){
+				if ((strings[j].toLowerCase()).compareTo((strings[j+1].toLowerCase())) > 0){
+temp = strings[j];
+strings[j] = strings[j+1];
+strings[j+1] = temp;
+hasSwitched = true;
+				}
+	
+			}
+		}
 	}
 
 }
